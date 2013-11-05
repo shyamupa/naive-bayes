1e431d20b496bb68346f6fd99519e25e7f9b1c91
Shyam Upadhyay <shyamupa@gmail.com>
2013-11-05 12:06:45 -0600

    Buggy!

--- src/main/java/Home/NaiveBayes.java
+++ src/main/java/Home/NaiveBayes.java
@@ -8,32 +8,65 @@ public class NaiveBayes implements Classifier
 {
 	private DataSet trainSet;	// must have training data
     private HashMap<String,Double> Priors;
+    private HashMap<String,HashMap<String,Double>> Likelihood; // first index is Label, second index is FeatureValue
+    private HashMap<String,Double>FeatureCount;
     public NaiveBayes(DataSet Train)
     {
     	trainSet=Train;
-    	
     }
+
     // Prob of each label
-    public void findPriors()
+    public void findPriorsAndLikelihood()
     {
+    	HashMap<String,Double> h;
     	for(Label l : trainSet.getLabels())
     	{
     		Priors.put(l.getName(), 0.0);
     	}
+    	
     	for(int i=0; i< trainSet.getSize();i++)
     	{
-    		String l=trainSet.getInstance(i).getLabel().getName();
+    		Instance d=trainSet.getInstance(i);
+    		String l=d.getLabel().getName();
     		Priors.put(l,Priors.get(l)+1.0);
+    		h=Likelihood.get(l);
+    		for(Feature f: d.getFeatures())
+    		{
+    			if(!h.containsKey(f.getValue()))	// no key with feature value present
+    				h.put(f.getValue(),1.0);
+    			else
+    				h.put(f.getValue(),h.get(f.getValue())+1.0);
+    			if(!FeatureCount.containsKey(f.getValue()))
+    				FeatureCount.put(f.getValue(),1.0);
+    			else
+    				FeatureCount.put(f.getValue(),FeatureCount.get(f.getValue())+1.0);
+    				
+//    			System.out.println(l+":"+f.getValue()+":"+h.get(f.getValue()));
+//    			System.out.println(f.getValue()+":"+FeatureCount.get(f.getValue()));
+    			
+    		}
     	}
+    	
     	for(Label l : trainSet.getLabels())
     	{
     		Priors.put(l.getName(),Priors.get(l.getName())/trainSet.getSize());
-    		System.out.println(Priors.get(l.getName()));
+    		//System.out.println(Priors.get(l.getName()));
     	}
     }
-    public void findLikeLihood()
+    public Double getLikelihood(String l, String FeatureVal)
     {
-    	
+    	Double lh,temp;
+    	HashMap<String,Double> h=Likelihood.get(l);
+    	//System.out.println(h.get(FeatureVal)+":"+FeatureVal);
+
+    	if(h.get(FeatureVal)==null)
+    		temp=0.0;
+    	else
+    		temp=h.get(FeatureVal);
+    		
+    	lh=temp/FeatureCount.get(FeatureVal);
+    	//System.out.println(lh);
+    	return lh;
     }
     
     public Label classify(Instance i)
@@ -55,32 +88,49 @@ public class NaiveBayes implements Classifier
     // finds P(Label | instance)
     private double getPosterior(Label label, Instance i)
     {
-    	double post=0.0;
-    	return post;
+    	double post=Priors.get(label.getName());
     	// Bayes Theorem
     	// Posterior P (Label | instance) ~ Prior P(Label) * Likelihood P(instance | Label)
-    	//post=Priors.get(label.getName()) * Likelihood.()
+    	for(Feature f: i.getFeatures())
+    	{
+    		post*=getLikelihood(label.getName(),f.getValue());
+    		
+    	}
+    	return post;
+    		
     }
     
     
 	public void train() 
 	{
 		Priors=new HashMap<String, Double>(trainSet.getLabels().size());
-		findPriors();	
+		Likelihood=new HashMap< String, HashMap<String, Double>>();
+		FeatureCount=new HashMap<String,Double>();
+		for(Label l: trainSet.getLabels())
+			Likelihood.put(l.getName(),new HashMap<String,Double>());
+		findPriorsAndLikelihood();	
 	}
-	public void test(String testfile) 
+	public void test(DataSet testSet) 
 	{
-		
+		for(int i=0;i<testSet.getSize();i++)
+		{
+			Instance d=testSet.getInstance(i);
+			System.out.println(this.classify(d).getName());
+		}
 	}
 	
-	public void test() {
+	public void test() 
+	{
 		
 	}
 	public static void main( String[] args ) throws IOException
 	{
-		DataSet ds=new DataSet("data/train_data.txt");
-		NaiveBayes nb= new NaiveBayes(ds);
+		DataSet ds1=new DataSet("data/train_data.txt");
+		NaiveBayes nb= new NaiveBayes(ds1);
 		nb.train();
-		
+		//nb.getLikelihood(">50K", "Female");
+		DataSet ds2=new DataSet("data/test_data.txt");
+		nb.test(ds2);
+		//nb.getPosterior(">50K","Female");
 	}
 }
